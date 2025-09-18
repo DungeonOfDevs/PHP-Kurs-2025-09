d775088585e8754b969d37233b42ed78d6955411
Blake <v.blake.dev@gmail.com>
2025-09-18 15:13:20 +0200

    PHP

--- 200-290/260_phpinfo.php
+++ 200-290/260_phpinfo.php
@@ -1,3 +1,17 @@
 <?php
 
 phpinfo();
+
+/*
+ * Apache-Webserver Konfigurationsdatei:
+ * C:\xampp\conf\httpd.conf
+ *
+ * PHP-Konfigurationsdatei:
+ * C:\xampp\php\php.ini
+ *
+ * Um Debuggen in PHP zu aktivieren,
+ * muss man die PHP-Erweiterung XDEBUG einbinden.
+ * Das geht heutzutage am besten direkt über die Settings von PHPStorm.
+ *
+ */
+
--- Filmverwaltung/020_Filme_anzeigen/config/filme_anzeigen.php
+++ Filmverwaltung/020_Filme_anzeigen/config/filme_anzeigen.php
@@ -10,12 +10,20 @@ $stmt = $db->query('SELECT * FROM filme;');
 $filme = $stmt->fetchAll();
 # var_dump($filme);
 
-var_dump($filme[0]); # array(9)
+# var_dump($filme[0]); # array(9)
 
-echo $filme[0]['titel'];    # Equalizer
+# echo $filme[0]['titel'];    # Equalizer
 
 $headings = array_keys($filme[0]);
-var_dump($headings);
+# var_dump($headings);
+
+$headings = array_map('ucfirst', $headings); // Anfangsbuchstaben sind jetzt groß ucfirst = erster Buchstabe
+
+# ID nicht anzeigen lassen für User
+foreach($filme as $key =>  $film) {
+    unset($film['id']);
+    $filme[$key] = $film;           # IDs sind weg aber heading ID noch da, muss an andere Stelle nachgearbeitet werden
+};
 
 ?>
 
@@ -24,11 +32,33 @@ var_dump($headings);
 <head>
     <meta charset="UTF-8">
     <title>Filme anzeigen</title>
+    <link href="style.css" rel="stylesheet">
 </head>
 <body>
 
 <h1>Filme anzeigen</h1>
 
+<table>
+    <tr>
+        <?php foreach($headings as $heading) {?>
+        <th><?php echo $heading; ?></th>
+        <?php } ?>
+    </tr>
+
+    <?php foreach($filme as $film) {?>
+    <tr>
+        <?php foreach ($film as $f) { ?>
+
+        <td>
+            <?php echo $f; ?>
+        </td>
+        <?php }?>
+
+    </tr>
+    <?php }?>
+</table>
+
+<!-- TODO: ID-Heading raus, Fsk -> FSK -->
 
 </body>
 </html>
